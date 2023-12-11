88b2db1a3b8d0ccd9f86aa1387b91a699af94f0c
Eric <3956301+ericgrandt@users.noreply.github.com>
2023-12-10 21:18:29 -0600

    Initial commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+require("eric")
+
+vim.o.expandtab = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+vim.o.wrap = false
+
+vim.o.hlsearch = false
+vim.incsearch = true
+
+vim.o.termguicolors = true
